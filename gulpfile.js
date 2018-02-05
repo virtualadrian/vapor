12bf5c247d78849d2de81c7e7fb4b9f395b1012c
MichelML <michmoreau.l@gmail.com>
2018-02-05 13:43:51 -0500

    add lint and package-lock in clean

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,9 @@
 // --min                        Flag to enable minification                                         Default: false
 // --gzip                       Flag to enable gziphication                                         Default: false
 
-var gulp = require('gulp-help')(require('gulp'));
-var requireDir = require('require-dir');
-var tasks = requireDir('./gulpTasks');
+let gulp = require('gulp-help')(require('gulp'));
+let requireDir = require('require-dir');
+let tasks = requireDir('./gulpTasks');
 
 gulp.task('default', 'Build sass, libs and sprites to the dist folder', [
     'sass',
@@ -12,10 +12,10 @@ gulp.task('default', 'Build sass, libs and sprites to the dist folder', [
     'copy:images',
     'copy:fonts',
     'copy:js',
-    'svg'
+    'svg',
 ]);
 
-gulp.task('watch', function () {
+gulp.task('watch', () => {
     gulp.watch('./scss/**/*', ['docs']);
     gulp.watch('./resources/js/**/*', ['copy:js']);
 });
