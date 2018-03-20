b9d4f28bd37a9557a55b70e007e8502286c93b64
Jean-Félix Thibodeau <thibodeaujf@gmail.com>
2018-03-20 16:00:09 -0400

    Add script that parses color palette, runs before sass in gulp

---
+++ gulpTasks/palette.js
@@ -0,0 +1,20 @@
+const gulp = require('gulp-help')(require('gulp'));
+const fs = require('fs');
+const _ = require('underscore');
+
+gulp.task('palette', 'Generates the palette map with all the color variables', (done) => {
+    const from = './scss/common/palette.scss';
+    const to = './scss/common/palette-map.scss';
+    const paletteFileContent = fs.readFileSync(from, 'utf8');
+    const sassVariables = paletteFileContent.match(/\$.+(?=:)/g); //.exec(fileContent);
+
+    let paletteMap = '$palette: (\n';
+
+    _.each(sassVariables, (sassVariable) => {
+        paletteMap += `    ${sassVariable.slice(1)}: ${sassVariable},\n`;
+    });
+
+    paletteMap += ');';
+
+    fs.writeFile(to, paletteMap, done);
+});
